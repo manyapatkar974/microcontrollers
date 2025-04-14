23e16295840f3509ae5fec9ac73c427b7f35cf4d
manyapatkar974 <manya.23cs070@sode-edu.in>
2025-04-14 18:27:12 +0530

    Delete Assignment directory

--- Assignment/Qn1.asm
+++
@@ -1,9 +0,0 @@
-    AREA PRG1, CODE, READONLY  
-    ENTRY                    
-    MOV r1, #0        
-    MOV r3, #12     
-    MVN r0, r1                         
-    AND r4, r0, r2                                            
-                    
-
-    END             ; 
--- Assignment/Qn14(in assembly).asm
+++
@@ -1,8 +0,0 @@
-    AREA Qn14, CODE, READONLY
-    ENTRY
-
-square_function
-    MUL R0, R0, R0  ; 
-    BX  LR          ; 
-
-    END
